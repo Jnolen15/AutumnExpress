0360e4a6b9f703d63602535e487bb9848501b157
Jared Nolen <Legodoghead@gmail.com>
2022-09-13 11:50:31 -0700

    Tram layout + Camera movement
    - New tram scene
    - blocked out potential tram layout in unity
    (Just layout not controls yet)
    - Added basic camera controlling
    -- Move mouse to edges of the screen to transition where you are looking

---
+++ Autumn Express/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Transform lookDefault;
+    public Transform lookDoor;
+
+    public enum Look
+    {
+        Default,
+        Door
+    }
+    public Look look;
+
+    [SerializeField] private float edgeSize;
+    [SerializeField] private float lookSpeed;
+    private bool moving = false;
+
+    void Update()
+    {
+        if (!moving)
+        {
+            if (Input.mousePosition.x > Screen.width - edgeSize && look != Look.Door)
+            {
+                look = Look.Door;
+                StartCoroutine(TransitionLook(lookDoor));
+            }
+
+            if (Input.mousePosition.x < edgeSize && look != Look.Default)
+            {
+                look = Look.Default;
+                StartCoroutine(TransitionLook(lookDefault));
+            }
+        }
+    }
+
+    IEnumerator TransitionLook(Transform lookto)
+    {
+        moving = true;
+        float time = 0;
+
+        Vector3 startPos = mainCamera.transform.position;
+        Quaternion startRot = mainCamera.transform.rotation;
+
+        while (time < lookSpeed)
+        {
+            float t = time / lookSpeed;
+            t = t * t * (3f - 2f * t);
+
+            mainCamera.transform.position = Vector3.Lerp(startPos, lookto.position, t);
+            mainCamera.transform.rotation = Quaternion.Lerp(startRot, lookto.rotation, t);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        moving = false;
+    }
+}
